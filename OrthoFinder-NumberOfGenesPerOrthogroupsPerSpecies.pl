303b024cafb17fa5e4694aa4e4203d89832e4645
npc <pierre_charrier@ymail.com>
2017-02-10 11:03:48 +0100

    OrthoFinder utility : is Wrapping Orthofinder matrix nammed "Orthogroups.csv" and count number of gene per orthogroup per specie.

---
+++ OrthoFinder-NumberOfGenesPerOrthogroupsPerSpecies.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#Description :
+my $usage ;
+$usage= "$0 <OG Matrix from Orthofinder>\n\n" ;
+$usage.= "Description : This script is producing a report of the number of genes per orthogroup and per specie\n" ;
+$usage.= "Dependency : <OG Matrix from Orthofinder> is a matrix with Orthogroup as row (i) and species as column (j)\n" ;
+$usage.= "	       Mij is a list of genes separated by a comma (,) \n" ;
+
+
+#my $OrthogroupFile = "Orthogroups.csv";
+my $OrthogroupFile = $ARGV[0];
+if (not defined $OrthogroupFile){
+	die $usage ;
+}
+
+open IN, "<$OrthogroupFile" or die "Unable to open \"$OrthogroupFile\" !\n";
+my %result ;
+# Wrapping row by row ($i)
+while(my $i = <IN>){
+  chomp $i;
+  my @sp = split "\t", $i ;
+  print $sp[0] ;
+  if($sp[0]!~ /^OG/){
+      for my $j (1 .. $#sp) {print "\t",$sp[$j]}
+  }
+  else{
+      # Specie by Specie ($j)
+      for my $j (1 .. $#sp) {
+	    my @seq = split /,/, $sp[$j] ;
+	    my $NumSeq = @seq ;
+	    print "\t",$NumSeq ;
+      }
+  }
+  print "\n" ;
+}
