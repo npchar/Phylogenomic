332f19e47795f02faf7dcf35a2c1864d9c79d06c
npc <pierre_charrier@ymail.com>
2018-01-09 11:13:31 +0100

    Add -p option to generate a partition file readable by PartitionFinder2

--- ConcatAlignement.R
+++ ConcatAlignement.R
@@ -9,8 +9,12 @@ library("ape")
 option_list = list(
   make_option(c("-f", "--file"), type="character", default=NULL,
               help="file with individual alignement file names (or locations)", metavar="character"),
+  make_option(c("-g", "--graphical"), action="store_true", default=FALSE,
+              help="produce a fancy graphical representation of the alignement"),
   make_option(c("-o", "--out"), type="character", default=NULL,
-              help="output alignement prefix", metavar="character")
+              help="output alignement prefix", metavar="character"),
+  make_option(c("-p", "--partionFinder"), action="store_true", default=FALSE,
+	      help="produce a partion file ready to ready by partion Finder")
   );
 
 opt_parser = OptionParser(option_list=option_list);
@@ -43,13 +47,25 @@ for(i in seq(1, length(listALN$V1))){
 	row.names(new) = gsub("_.*", "", labels(new))
 	if(init==0 & dim(new)[2]!=0){ 
 		bindAln = new
-		PartFile = paste("DNA, gene",as.character(i),"=1-",as.character(dim(new)[2]), sep='') 
+		if(opt$partionFinder){
+			PartFile = paste("gene",as.character(i), "_pos1 ","= 1-",as.character(dim(new)[2]), as.character('\\3;'), sep='')
+			PartFile = c( PartFile , paste("gene",as.character(i), "_pos2 ","= 2-",as.character(dim(new)[2]), as.character('\\3;'), sep='') )
+			PartFile = c( PartFile , paste("gene",as.character(i), "_pos3 ","= 3-",as.character(dim(new)[2]), as.character('\\3;'), sep='') )
+		} else {
+			PartFile = paste("DNA, gene",as.character(i),"=1-",as.character(dim(new)[2]), sep='') 
+		}
 		init=1
 	}
 	else{ 
 		lastPos = dim(bindAln)[2]
 		bindAln = cbind(bindAln, new, fill.with.gaps=TRUE,check.names=TRUE) 
-		PartFile = c(PartFile, paste("DNA, gene",as.character(i),"=",as.character(lastPos + 1), "-",as.character(lastPos + dim(new)[2]), sep='' ))
+		if(opt$partionFinder){
+			PartFile = c(PartFile, paste("gene",as.character(i),"_pos1 ", "= ",as.character(lastPos + 1), "-",as.character(lastPos + dim(new)[2]), as.character('\\3;'), sep='' ))
+			PartFile = c(PartFile, paste("gene",as.character(i),"_pos2 ", "= ",as.character(lastPos + 2), "-",as.character(lastPos + dim(new)[2]), as.character('\\3;'), sep='' ))
+			PartFile = c(PartFile, paste("gene",as.character(i),"_pos3 ", "= ",as.character(lastPos + 3), "-",as.character(lastPos + dim(new)[2]), as.character('\\3;'), sep='' ))
+		} else {
+			PartFile = c(PartFile, paste("DNA, gene",as.character(i),"=",as.character(lastPos + 1), "-",as.character(lastPos + dim(new)[2]), sep='' ))
+		}
 	}
 	
 }
@@ -58,11 +74,15 @@ for(i in seq(1, length(listALN$V1))){
 # concatenate the alignments, checking the names of the sequences in case samples order varies between alignments, and padding out the final alignment with gaps if any samples are missing from the individual alignments
 #c=cbind(a,b,fill.with.gaps=TRUE,check.names=TRUE)
 
-# have a look at your concatenated aln
-pdf(file=paste(opt$out,"visualAlignment.pdf", sep='.'))
-image.DNAbin(bindAln)
-dev.off()
-
 # write the concat aln to a new fasta, without any column spacers or anything else between nucleotides
 write.dna(bindAln, file=paste(opt$out,"fasta", sep='.'), format="fasta", colsep="")
 write.table(PartFile, file=paste(opt$out, "partition",sep='.'), quote=F, col.names=F, row.names=F)
+
+
+# have a look at your concatenated aln
+if(opt$graphical){
+	pdf(file=paste(opt$out,"visualAlignment.pdf", sep='.'))
+	image.DNAbin(bindAln)
+	dev.off()
+}
+
